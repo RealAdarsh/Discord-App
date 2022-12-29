5f9812c02f93111060161d33edaf69c0c17f4d75
realAdarsh <areadarsh@outlook.com>
2022-12-29 17:02:58 +0530

    adding request validation with joi

--- discord-backend/routes/authRoutes.js
+++ discord-backend/routes/authRoutes.js
@@ -2,9 +2,28 @@ const express = require("express");
 const router = express.Router(); 
 const authControllers = require("../controllers/auth/authController");
 
-router.post("/register", authControllers.controllers.postRegister); 
+// validate that correct data is send using joi package 
 
-router.post("/login", authControllers.controllers.postLogin); 
+const Joi=require('joi'); 
+const validator=require('express-joi-validation').createValidator({}); 
+
+const registerSchema=Joi.object({
+    // minimum length for username =3 and maximum 12 and password has minimum=6 and max=12
+    username: Joi.string().min(3).max(12), 
+    password: Joi.string().min(6).max(12),
+    mail: Joi.string().email(),
+});
+
+const loginSchema=Joi.object({
+    password: Joi.string().min(6).max(12),
+    mail: Joi.string().email(),   
+})
+
+// validator is used to validate that correct data is send 
+
+router.post("/register", validator.body(registerSchema) , authControllers.controllers.postRegister); 
+
+router.post("/login", validator.body(loginSchema),  authControllers.controllers.postLogin); 
 
 
 module.exports = router; 

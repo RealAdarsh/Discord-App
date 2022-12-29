5af56c1d93ebbd215a9eb890ca2f9fd6ec04fc43
realAdarsh <areadarsh@outlook.com>
2022-12-29 18:40:22 +0530

    created JWT Tokens

--- discord-backend/controllers/auth/postRegister.js
+++ discord-backend/controllers/auth/postRegister.js
@@ -1,43 +1,53 @@
-const User=require("../../models/user"); 
-const bcrypt = require("bcryptjs"); 
+const User = require("../../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const postRegister= async(req,res) => {
-    try{
-        const{ username, mail, password }=req.body; 
-        // check if user exists 
-        const userExists= await User.exists({ mail: mail.toLowerCase()});
-        
-        if (userExists){
-            return res.status(409).send('Email already in use.'); 
-        } 
+const postRegister = async (req, res) => {
+  try {
+    const { username, mail, password } = req.body;
+    // check if user exists
+    const userExists = await User.exists({ mail: mail.toLowerCase() });
 
-        // encrypt the password
-        
-        const encryptedPassword = await bcrypt.hash(password,10); 
-
-        // create user document and save it in the database 
-
-        const user = await User.create({
-            username,
-            mail: mail.toLowerCase(),
-            password:encryptedPassword
-        });
-
-        // create JWT Token 
-
-        const token='JWT TOKEN'; 
-
-        return res.status(201).json({
-            userDetails: {
-                mail: user.mail, 
-                token: token,
-                username: user.username,
-            }
-        });
-
-    } catch(err){
-        return res.status(500).send('Error Occured. Please try again'); 
+    if (userExists) {
+      return res.status(409).send("Email already in use.");
     }
+
+    // encrypt the password
+
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    // create user document and save it in the database
+
+    const user = await User.create({
+      username,
+      mail: mail.toLowerCase(),
+      password: encryptedPassword,
+    });
+
+    // create JWT Token
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        mail,
+      },
+
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    return res.status(201).json({
+      userDetails: {
+        mail: user.mail,
+        token: token,
+        username: user.username,
+      },
+    });
+  } catch (err) {
+    return res.status(500).send("Error Occured. Please try again");
+  }
 };
 
-module.exports=postRegister; 
+module.exports = postRegister;

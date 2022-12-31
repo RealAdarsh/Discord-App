8c4f51ff6776d6bd8ab0248e83227439b61ef1bc
realAdarsh <areadarsh@outlook.com>
2022-12-31 14:07:46 +0530

    added routing to the page

--- discord-frontend/src/index.js
+++ discord-frontend/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
+import "./index.css";
+import App from "./App";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./index.css";
 import store from "./store/store";
 
 const container = document.getElementById("root");

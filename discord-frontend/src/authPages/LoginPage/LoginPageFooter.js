ada05caa938615cea85eed08e85081457e2e9da8
realAdarsh <areadarsh@outlook.com>
2022-12-31 20:43:31 +0530

    created login page

--- discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
+++ discord-frontend/src/authPages/LoginPage/LoginPageFooter.js
@@ -2,8 +2,17 @@ import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
 import RedirectInfo from "../../shared/components/RedirectInfo";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
 // responsible for submitting the logini request
+
+const getFormNotValidMessage = () => {
+  return "Enter correct e-mail address and password should contains between 6 and 12 characters ";
+};
+
+const getFormValidMessage = () => {
+  return "Press to log in!";
+};
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
   const navigate = useNavigate();
@@ -13,18 +22,23 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
 
   return (
     <>
-      <div>
-        <CustomPrimaryButton
-          label="Log in"
-          additionalStyles={{ marginTop: "30px" }}
-          disabled={!isFormValid}
-          onClick={handleLogin}
-        />
-      </div>
+      <Tooltip
+        title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}
+      >
+        <div>
+          <CustomPrimaryButton
+            label="Log in"
+            additionalStyles={{ marginTop: "30px" }}
+            disabled={!isFormValid}
+            onClick={handleLogin}
+          />
+        </div>
+      </Tooltip>
+
       <RedirectInfo
-        text="Need an accout? "
+        text="Need an account? "
         redirectText="Create an account"
-        additionalStyle={{ marginTop: "5px" }}
+        additionalStyles={{ marginTop: "5px" }}
         redirectHandler={handlePushToRegisterPage}
       />
     </>
